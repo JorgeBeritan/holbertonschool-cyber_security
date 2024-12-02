00f000621353f850f261da871289ba4cedd3a306
JorgeBeritan <inaikmcontacto@gmail.com>
2024-12-02 06:02:15 -0500

    add no error msg

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-if sha256sum -c <(echo "$2 $1") 2>/dev/null | grep -q ": OK$"; then echo "$1: OK"; else echo "$1: INVALID"; fi
+if sha256sum -c <(echo "$2 $1") 2>/dev/null | grep -q ": OK$"; then echo "$1: OK"; fi
