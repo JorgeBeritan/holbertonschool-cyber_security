fda3f25c7e67672c2813eb43667ddcf776c58d61
JorgeBeritan <inaikmcontacto@gmail.com>
2025-01-19 06:00:43 -0500

    First commit

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -5,14 +5,23 @@ resultado=""
 
 if [[ "$word" =~ \{xor\} ]]; then
   clean_word="${word#\{xor\}}"
-  bword=$(echo -n "$clean_word" | base64 -d 2>/dev/null)
-  
-  for (( i=0; i<${#bword}; i++)); do
-    char="${bword:i:1}"
-    ascii=$(printf "%d" "'$char'")
-    xor=$((ascii ^ 95))
-    new_char=$(printf "\\$(printf '%03o' "$xor")")
-    resultado+="$new_char"
-  done
-  echo "$resultado"
+  # Decodificar con Base64 y eliminar caracteres nulos
+  bword=$(echo -n "$clean_word" | base64 -d 2>/dev/null | tr -d '\0')
+
+  if [[ -n "$bword" ]]; then
+    for (( i=0; i<${#bword}; i++)); do
+      char="${bword:i:1}"
+      ascii=$(printf "%d" "'$char'")
+      xor=$((ascii ^ 95))
+      # Eliminar caracteres nulos antes de la conversión a octal
+      new_char=$(printf "%03o" "$xor" | tr -d '\0')
+      new_char=$(printf "\\$new_char") 
+      resultado+="$new_char"
+    done
+    echo "$resultado"
+  else
+    echo "Error al decodificar la cadena en Base64."
+  fi
+else
+  echo "La cadena no contiene la etiqueta {xor}."
 fi
