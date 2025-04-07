6893f2b2328b0eb3a94f8b1348afb1c5d95c037f
JorgeBeritan <inaikmcontacto@gmail.com>
2025-04-06 21:33:11 -0300

    First commit

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ATTACKER=$(grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')
-grep "$ATTACKER" logs.txt | grep -oE '"[^"]*User-Agent[^"]"' | sed 's/.*User-Agent: \([^"]*\).*/\1/' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print substr($0x2)}'
+grep "$ATTACKER" logs.txt | grep -oE '"[^"]*User-Agent[^"]"' | sed 's/.*User-Agent: \([^"]*\).*/\1/' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print substr($0,2)}'
