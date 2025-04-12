0b0f9c89bb08dd19a5ea578d312418f25e00b3cd
JorgeBeritan <inaikmcontacto@gmail.com>
2025-04-12 08:13:06 -0300

    All in one line

--- network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-nmap -p 80,443 --script "http-vuln-cve2017-5638" $1
-nmap -p 80,443 --script "ssl-enum-sciphers" $1
-nmap -p 21, --script "ftp-anon" $1
+nmap -p 21,80,443 --script "http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon" $1
