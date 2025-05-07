4eb25464e0132bc62e8cd9749eb68eed44bf9d93
JorgeBeritan <inaikmcontacto@gmail.com>
2025-05-07 08:42:32 -0300

    First commit

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+require "json"
+
+def count_user_id(path)
+    user_counts = {}
+    contenido_json = File.read(path)
+
+    datos = JSON.parse(contenido_json)
+
+    if datos.is_a?(Array)
+        datos.each do |elemento|
+            if elemento.is_a?(Hash) && elemento.key?('userId')
+                user_id = elemento['userId']
+                next if user_id.nil?
+                user_id = user_id.to_s
+                user_counts[user_id] = (user_counts[user_id] || 0) + 1
+            end
+        end
+    else
+        puts "Algo salio mal"
+        return {}
+    end
+    user_counts.each do |user_id, count|
+        puts "#{user_id}: #{count}"
+    end
+end
+
