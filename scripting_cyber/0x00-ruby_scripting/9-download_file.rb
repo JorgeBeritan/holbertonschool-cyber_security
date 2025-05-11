5d09d49372f1c77c51efbde356bc036c76e13cc5
JorgeBeritan <inaikmcontacto@gmail.com>
2025-05-11 09:55:07 -0300

    First commit

---
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+require 'open-uri'
+require 'fileutils'
+require 'uri'
+
+def download_file(url_string, local_file_path)
+    uri = URI.parse(url_string)
+    unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+        puts "Error: Invalid URL provided. Please use http or https"
+        return
+    end
+
+    puts "Downloading file from #{url_string}..."
+
+    URI.open(url_string) do |remote_file|
+        FileUtils.mkdir_p(File.dirname(local_file_path))
+
+        File.open(local_file_path, 'wb') do |local_file|
+            local_file.write(remote_file.read)
+        end
+    end
+
+    puts "File downloaded and saved to #{local_file_path}"
+end
