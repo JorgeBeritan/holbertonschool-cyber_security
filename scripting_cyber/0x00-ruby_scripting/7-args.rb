78fc375bdbf2f9157df2d66c57776bd482ec3bf4
JorgeBeritan <inaikmcontacto@gmail.com>
2025-05-11 08:21:12 -0300

    First commit

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -4,8 +4,8 @@ def print_arguments
     if ARGV.empty?
         puts "No arguments provided."
     else
+        puts "Arguments:"
         ARGV.each_with_index do |args, index|
-            puts "Arguments:"
             puts "#{index + 1}. #{args}"
         end
     end
